be67109d9ef6188684a5ed05fca1768886c8ca6a
Chilli <chilli.mahendra@zoopla.co.uk>
2021-11-02 10:54:13 +0000

    connect to database

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BCS_quizmaster_2021
 Synoptic project for BCS
+
+
+## To run the server: 
+`DEBUG=myapp:* npm start`
+
+navigate to http://localhost:3000/
---
+++ db.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+// get client
+const mysql = require('mysql2');
+
+// create connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'bcs_user',
+    password: process.env.DB_PASSWORD,
+    database: 'bcs_quizmaster'
+});
+
+// console.log to show successful connection
+connection.connect(function(error){
+    if(!!error) {
+        console.log(error);
+    } else {
+        console.log('Connected!');
+    }
+});
+
+// create query
+function query(sql) {
+    const results = connection
+        .promise()
+        .query(sql)
+        .then(([rows]) => {
+            return rows;
+        })
+    return results;
+}
+
+module.exports.query = query;
